a6f7467cda6ecc408560adf7015fd0d4d279bbcd
HolbertonFalcon <144282143+HolbertonFalcon@users.noreply.github.com>
2023-11-06 23:06:48 -0600

    official comit for printf

--- main.h
+++ main.h
@@ -1,9 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+int _putchar(char c);
 int _printf(const char *format, ...);
-
+int print_char(char c, int *char_print);
+int print_string(char *str, int *char_print);
+int print_integer(int num, int *char_print);
+int print_binary(unsigned int num, int *char_print);
 #endif
