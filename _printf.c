f36d0bda2a3c5f8b4e4dd23903ac105aa75524da
HolbertonFalcon <144282143+HolbertonFalcon@users.noreply.github.com>
2023-11-07 02:05:22 -0600

    *

--- _printf.c
+++ _printf.c
@@ -98,17 +98,16 @@ int _printf(const char *format, ...)
 			if (*format == '\0')
 				return (-1);
 			if (*format == '%')
-            {
-                print_char('%', &char_print);
-                format++;
-            }
-        }
-        else
-        {
-            print_char(*format, &char_print);
-        }
-        format++;
-    }
+			{
+				print_char('%', &char_print);
+			}
+			else
+			{
+    
+    			print_char(*format, &char_print);
+
+    			format++; 
+			}
 			else if (*format == 'c')
 				print_char(va_arg(arg, int), &char_print);
 
